32e7b873616dbf70f747531171762e95c4dae1c6
Lalo <work@lalo.im>
2023-08-09 17:29:48 +0800

    聊天对话的提示信息

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -32,9 +32,21 @@ store.$subscribe((_, state) => {
     <my-chat-bot class="flex-none w-60 overflow-auto" :botInfo="botInfo"></my-chat-bot>
     <!-- 聊天 -->
     <div class="grow flex flex-col gap-1 bg-slate-200">
+      <!-- 对话列表 -->
       <div ref="chatlist" class="grow overflow-auto px-4">
-        <my-chat-list></my-chat-list>
+        <my-chat-list v-if="store.conversation.length > 0"></my-chat-list>
+        <!-- 默认提示 -->
+        <div v-else class="grow hero">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h1 class="text-5xl font-bold">AppChain.AI</h1>
+              <p class="py-6">创建自己的 AI 代理，连接 LLM 并访问不同的接口。</p>
+            </div>
+          </div>
+        </div>
       </div>
+      
+      <!-- 发送消息 -->
       <div class="grow-0 px-4">
         <my-chat-send></my-chat-send>
       </div>
