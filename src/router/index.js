3888455c408972287247afa60ebf1a3a302179b3
Lalo <work@lalo.im>
2023-08-25 13:53:40 +0800

    整合chatbase

--- src/router/index.js
+++ src/router/index.js
@@ -37,6 +37,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('@/views/FavoritesView.vue')
+    },
+    {
+      path: '/help',
+      name: 'help',
+      component: () => import('@/views/HelpView.vue')
     }
   ]
 })
